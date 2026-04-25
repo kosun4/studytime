70cf258f2fd75e5c0f9dc4822a3616d7a1694896
kosun4 <kosuna0706@gmail.com>
2026-04-25 20:28:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const URL = "あなたのGASのURL"; // ★ここに自分のURLを貼る！
+const URL = "https://script.google.com/macros/s/AKfycby3pv5UxD6FwT3vv2g2HY_WRp8_QIYIp0ecVSC6U0fvHw0lDOJ8IPj_18P34DVCwdkc/exec"; // ★ここに自分のURLを貼る！
 
 let startTime, timerInterval, elapsedTime = 0;
 const timerDisplay = document.getElementById('timerDisplay');
